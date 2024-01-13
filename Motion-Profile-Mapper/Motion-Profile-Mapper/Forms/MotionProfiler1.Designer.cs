8e8cd723d4239bd42fe855b11b9509e10b014cf9
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2024-01-13 17:48:53 -0500

    WORK! PLEASE

--- Motion-Profile-Mapper/Motion-Profile-Mapper/Forms/MotionProfiler1.Designer.cs
+++ Motion-Profile-Mapper/Motion-Profile-Mapper/Forms/MotionProfiler1.Designer.cs
@@ -39,7 +39,7 @@ namespace MotionProfileMapper.Forms {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VelocityMap.Forms.MotionProfiler", typeof(MotionProfiler).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MotionProfileMapper.Forms.MotionProfiler", typeof(MotionProfiler).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Motion-Profile-Mapper/Motion-Profile-Mapper/Properties/Resources.Designer.cs
+++ Motion-Profile-Mapper/Motion-Profile-Mapper/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace MotionProfileMapper.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VelocityMap.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MotionProfileMapper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
