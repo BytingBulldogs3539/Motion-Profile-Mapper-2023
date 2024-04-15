4bf21138ada1645529a240e6ef019ec9fc667147
Devon Doyle <devondoyle@outlook.com>
2024-04-15 19:12:04 -0400

    setup

---
+++ BulldogTools/windows/BulldogTools/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::BulldogTools::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::BulldogTools::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
